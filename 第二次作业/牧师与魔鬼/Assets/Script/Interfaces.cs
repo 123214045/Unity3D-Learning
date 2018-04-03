0de55057ffba556042d699681fb209db4c4270a4
Unity3D-Learning <liux276@users.noreply.github.com>
2018-04-03 13:53:12 +0800

    3

---
+++ 第二次作业/牧师与魔鬼/Assets/Script/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interfaces
+{
+    public interface ISceneController
+    {
+        void LoadResources();
+    }
+
+    public interface UserAction
+    {
+        void MoveBoat();
+        void ObjectIsClicked(GameObjects characterCtrl);
+        void Restart();
+    }
+
+}
---
+++ 第二次作业/牧师与魔鬼/Assets/Script/Move.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    readonly float Speed = 20;
+
+    Vector3 Target;
+    Vector3 Middle;
+    int Move_State = 0;  // 0->no need to move, 1->object moving , 2->boat moving to dest
+    bool To_Middle = true;
+
+    void Update()
+    {
+        if (Move_State == 1)
+        {
+            if (To_Middle)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Middle, Speed * Time.deltaTime);
+                if (transform.position == Middle) To_Middle = false;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+                if (transform.position == Target) Move_State = 0;
+            }
+        }
+        else if (Move_State == 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+            if (transform.position == Target)
+            {
+                To_Middle = true;
+                Move_State = 0;
+            }
+        }
+    }
+
+    public void SetDestination(Vector3 Position)
+    {
+        if (Move_State != 0) return;
+        Target = Middle = Position;
+        To_Middle = true;
+        if (transform.position.y == Target.y)
+        {
+            Move_State = 2;
+        }
+        else
+        {
+            Move_State = 1;
+            if (transform.position.y < Target.y)
+            {
+                Middle.x = transform.position.x;
+            }
+            else if (transform.position.y > Target.y)
+            {
+                Middle.y = transform.position.y;
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        Move_State = 0;
+        To_Middle = true;
+    }
+}
